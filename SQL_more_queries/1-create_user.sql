0f6912a9eb886e00c0e4f12c0b63629efc63bfa8
Manuel Fajardo <39359956+Manuehh2@users.noreply.github.com>
2026-02-19 19:38:12 -0600

    Remove FLUSH PRIVILEGES from create user script
    Removed FLUSH PRIVILEGES command from user creation script.

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,4 @@
 -- Script 1
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
 
