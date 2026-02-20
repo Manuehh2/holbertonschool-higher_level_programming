703a91c7a308b80a6a906a9057f0957f20a44ca2
Manuehh2 <strilldrax@gmail.com>
2026-02-20 18:37:37 +0000

    terminamos

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,9 @@
+-- Task 14
+SELECT genres.name
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+JOIN genres ON tv_show_genres.genre_id = genres.id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY genres.name ASC;
+
+
