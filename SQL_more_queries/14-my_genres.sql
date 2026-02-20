800a9d8da48774bb4d6dfe0cce92a749cfa15122
Manuel Fajardo <39359956+Manuehh2@users.noreply.github.com>
2026-02-20 12:45:41 -0600

    Fix join condition for genre selection query

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,7 @@
 -- Task 14
 SELECT genres.name
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 JOIN genres ON tv_show_genres.genre_id = genres.id
 WHERE tv_shows.title = 'Dexter'
 ORDER BY genres.name ASC;
